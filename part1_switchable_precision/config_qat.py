2ebd2e44222e5d70c79a51eba5349209c5607772
Laurence Wu <your.email@example.com>
2025-09-16 15:15:24 -0400

    training fix

--- part1_switchable_precision/config_qat.py
+++ part1_switchable_precision/config_qat.py
@@ -54,7 +54,7 @@ class TrainingConfig:
         self.max_grad_norm = 1.0
 
         # Training schedule
-        self.num_iterations = 2000
+        self.num_iterations = 500
         self.gradient_accumulation_steps = 8
 
         # Evaluation
