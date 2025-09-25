c23b3c8cd69147396f52884808787b30f5cbd8e6
Laurence-Wu <1164842256@qq.com>
2025-09-24 23:45:37 -0400

    This is the auto push for the sake of running programs on the targeted environment and server. Please ignore this commit. If a serious commit is being pushed, please stash or rebase it.

--- part2_cyclic_precision_training/main_cpt.py
+++ part2_cyclic_precision_training/main_cpt.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from transformers import GPT2Tokenizer, GPT2Model
-from datasets import load_dataset
 import numpy as np
 from tqdm import tqdm
 import time
@@ -20,54 +19,7 @@ from config_cpt import get_config
 from cpt_model import CPTModel
 from cyclic_scheduler import CyclicPrecisionScheduler, PrecisionRangeTest
 from deploy import save_cpt_checkpoint, save_final_models
-
-
-class WikiTextDataset(torch.utils.data.Dataset):
-    """WikiText dataset for language modeling."""
-
-    def __init__(self, split: str, tokenizer, max_seq_length: int = 256, doc_stride: int = 128):
-        self.tokenizer = tokenizer
-        self.max_seq_length = max_seq_length
-        self.doc_stride = doc_stride
-
-        # Load dataset
-        dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split=split)
-        self.texts = dataset['text']
-
-        # Tokenize and create sequences
-        self.sequences = self._create_sequences()
-
-    def _create_sequences(self):
-        """Create overlapping sequences from texts."""
-        sequences = []
-        for text in self.texts:
-            if len(text.strip()) == 0:
-                continue
-
-            # Tokenize
-            tokens = self.tokenizer(
-                text,
-                truncation=False,
-                return_tensors='pt'
-            )['input_ids'][0]
-
-            # Create overlapping sequences
-            for i in range(0, len(tokens) - self.max_seq_length + 1, self.doc_stride):
-                seq = tokens[i:i + self.max_seq_length]
-                if len(seq) == self.max_seq_length:
-                    sequences.append(seq)
-
-        return sequences
-
-    def __len__(self):
-        return len(self.sequences)
-
-    def __getitem__(self, idx):
-        seq = self.sequences[idx]
-        return {
-            'input_ids': seq[:-1],
-            'labels': seq[1:]
-        }
+from dataset import WikiTextDataset
 
 
 def train_step_with_cyclic_precision(
