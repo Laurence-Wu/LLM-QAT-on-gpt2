1edcbad6f1e30f36889583059bd36494fbfb6250
Laurence-Wu <1164842256@qq.com>
2025-09-27 19:27:09 -0400

    This is the auto push for the sake of running programs on the targeted environment and server. Please ignore this commit. If a serious commit is being pushed, please stash or rebase it.

--- part1_switchable_precision/config_sp.py
+++ part1_switchable_precision/config_sp.py
@@ -72,7 +72,7 @@ class TrainingConfig:
         self.max_grad_norm = 1.0
 
         # Training schedule
-        self.num_iterations = 1000
+        self.num_iterations = 600
         self.gradient_accumulation_steps = 8
 
         # Evaluation
